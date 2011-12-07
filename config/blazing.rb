6ef5fce6ef6367237f816f8a1e162d05bb16ad78
Vichao <vichaos@gmail.com>
2011-12-07 13:57:40 +0700

    my second commit

---
+++ config/blazing.rb
@@ -0,0 +1,35 @@
+#
+# Blazing Configuration File
+#
+
+#
+# Specify the location of your repository:
+#
+repository 'git@github.com:vichaos/test_blazing.git'
+
+#
+# Define possible deploy targets:
+#
+# target :production, 'blazing@production.server.com:/where/your/code/is/shipped/to'
+# target :staging, 'blazing@production.server.com:/where/your/code/is/shipped/to', :default => true, :some_option => 'foo' # note: options defined here will override options defined globally for a recipe
+
+#
+# Optional: RVM handling
+#
+# Use Ruby Enterprise Edition and gemset 123
+rvm '1.9.2@test'
+
+# Or use the rvmrc
+# rvm :rvmrc
+#
+
+#
+# Define Recipes to be used (See the README for available recipes).
+# Recipes are run in the order they are specified
+#
+# recipe :recipe_name
+
+#
+# Define a rake task that should be run after deployment. The rake task will run after the recipes have run.
+#
+# rake :a_rake_task_to_call_after_deployment
