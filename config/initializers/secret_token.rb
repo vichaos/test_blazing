dd34a0c3cf4dfe54c3eab205aa2211288513c898
Vichao <vichaos@gmail.com>
2011-12-07 11:57:29 +0700

    my first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestBlazing::Application.config.secret_token = 'af5511ef19ef5b20f5989aa4f286e6faf83ad6bfcffc35db496afe0ff74d4efb479d509f8bd666455b0ec169f652f06f0c6a71a8e7888ef93dcfdf3fe28a4374'
