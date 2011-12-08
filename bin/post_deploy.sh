52ce4bca5f96683febbc9fd6b3a4ac3c2b5364a3
Vichao <vichaos@gmail.com>
2011-12-08 15:56:53 +0700

    .

--- bin/post_deploy.sh
+++ bin/post_deploy.sh
@@ -1,2 +1,2 @@
-[[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm" # Load RVM function
-rvm use 1.9.2@test
+#[[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm" # Load RVM function
+#rvm use 1.9.2@test
